39ecdfcf15b3a135651ab78318000e1ca194cd7c
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-04-11 12:52:09 +1000

    tigger/cluda: added basic Env classes for Cuda and OpenCL

--- tigger/cluda/cuda.py
+++ tigger/cluda/cuda.py
@@ -1,7 +1,111 @@
+import numpy
+import sys
+from pycuda.tools import make_default_context
+import pycuda.gpuarray as gpuarray
+import pycuda.driver as cuda
+from pycuda.compiler import SourceModule
+
+
 class CudaEnvironment:
 
-    def __init__(self, device_num=0, fast_math=True):
+    def __init__(self, device_num=0, fast_math=True, sync=False):
         self.fast_math = fast_math
+        cuda.init()
+        self.context = make_default_context()
+        self.params = CudaDeviceParameters(self.context.get_device())
+        self.stream = None if sync else cuda.Stream()
+        self.sync = sync
 
-    def supportsDouble(self):
-        return True
+        self._released = False
+
+    def supportsDtype(self, dtype):
+        if dtype in (numpy.float64, numpy.complex128):
+            major, minor = self.context.get_device().compute_capability()
+            return (major == 1 and minor == 3) or major >= 2
+        else:
+            return True # TODO: check if there are other limitations
+
+    def allocate(self, shape, dtype):
+        return gpuarray.GPUArray(shape, dtype=dtype)
+
+    def synchronize(self):
+        if not self.sync:
+            self.stream.synchronize()
+
+    def fromDevice(self, arr):
+        return arr.get()
+
+    def toDevice(self, arr):
+        if self.sync:
+            return gpuarray.to_gpu(arr)
+        else:
+            # FIXME: there must be a warning in docs that buf has to be pagelocked
+            return gpuarray.to_gpu_async(arr, stream=self.stream)
+
+    def compile(self, template, **kwds):
+        return CudaModule(self, template, **kwds)
+
+    def release(self):
+        if not self._released:
+            self.context.pop()
+            self._released = True
+
+    def __del__(self):
+        self.release()
+
+
+class CudaDeviceParameters:
+
+    def __init__(self, device):
+
+        self.max_block_size = device.get_attribute(cuda.device_attribute.MAX_THREADS_PER_BLOCK)
+        self.max_block_dims = [
+            device.get_attribute(cuda.device_attribute.MAX_BLOCK_DIM_X),
+            device.get_attribute(cuda.device_attribute.MAX_BLOCK_DIM_Y),
+            device.get_attribute(cuda.device_attribute.MAX_BLOCK_DIM_Z)
+        ]
+
+        self.max_grid_dims = [
+            device.get_attribute(cuda.device_attribute.MAX_GRID_DIM_X),
+            device.get_attribute(cuda.device_attribute.MAX_GRID_DIM_Y)
+        ]
+
+        self.smem_banks = 16 # FIXME: must get it from device
+        self.warp_size = device.get_attribute(cuda.device_attribute.WARP_SIZE)
+
+        self.api = 'cuda'
+
+
+class CudaModule:
+
+    def __init__(self, env, template, **kwds):
+        self._env = env
+        src = template.render(env=env.params, **kwds)
+        options = ['-use_fast_math'] if self._env.fast_math else []
+
+        try:
+            self._module = SourceModule(src, no_extern_c=True, options=options)
+        except:
+            for i, l in enumerate(src.split('\n')):
+                print i, ":", l
+            raise
+
+    def __getattr__(self, name):
+        return CudaKernel(self._env, self._module.get_function(name))
+
+
+class CudaKernel:
+
+    def __init__(self, env, kernel):
+        self._env = env
+        self._kernel = kernel
+
+    def __call__(self, *args, **kwds):
+        # Python 2.* cannot handle explicit keywords after variable-length positional arguments
+        block = kwds.pop('block', (1, 1, 1))
+        grid = kwds.pop('grid', (1, 1))
+        shared = kwds.pop('shared', 0)
+        assert len(kwds) == 0, "Unknown keyword arguments: " + str(kwds.keys())
+
+        self._kernel(*args, grid=grid, block=block, stream=self._env.stream)
+
--- tigger/cluda/ocl.py
+++ tigger/cluda/ocl.py
@@ -1,7 +1,105 @@
+import numpy
+import pyopencl as cl
+import pyopencl.array as clarray
+import sys
+
+
 class OclEnvironment:
 
-    def __init__(self, device_num=0, fast_math=True):
+    def __init__(self, device_num=0, fast_math=True, sync=False):
         self.fast_math = fast_math
 
-    def supportsDouble(self):
-        return False
+        platforms = cl.get_platforms()
+        target_device = None
+        for p in platforms:
+            devices = p.get_devices()
+            for d in devices:
+                params = OclDeviceParameters(d)
+                if params.max_block_size > 1:
+                    target_device = d
+                    break
+            if target_device is not None:
+                break
+
+        self.context = cl.Context(devices=[target_device])
+        self.queue = cl.CommandQueue(self.context)
+        self.sync = sync
+        self.params = OclDeviceParameters(self.context.get_info(cl.context_info.DEVICES)[0])
+
+    def supportsDtype(self, dtype):
+        if dtype in (numpy.float64, numpy.complex128):
+            extensions = self.context.devices[0].extensions
+            return "cl_khr_fp64" in extensions or "cl_amd_fp64" in extensions
+        else:
+            return True # TODO: check if there are other limitations
+
+    def allocate(self, shape, dtype):
+        return clarray.Array(self.queue, shape, dtype=dtype)
+
+    def fromDevice(self, arr):
+        return arr.get()
+
+    def toDevice(self, arr):
+        res = clarray.to_device(self.queue, arr)
+        if self.sync:
+            self.queue.finish()
+        return res
+
+    def release(self):
+        pass
+
+    def compile(self, template, **kwds):
+        return OclModule(self, template, **kwds)
+
+
+class OclDeviceParameters:
+
+    def __init__(self, device):
+        self.max_block_size = device.get_info(cl.device_info.MAX_WORK_GROUP_SIZE)
+        self.max_block_dims = device.get_info(cl.device_info.MAX_WORK_ITEM_SIZES)
+
+        self.max_grid_dims = [sys.maxint, sys.maxint]
+
+        self.smem_banks = 16 # FIXME: must get it from device
+        self.warp_size = 32 # FIXME: must get it from device
+
+        self.api = 'ocl'
+
+
+class OclModule:
+
+    def __init__(self, env, template, **kwds):
+        self._env = env
+        src = template.render(env=env.params, **kwds)
+        options = "-cl-mad-enable -cl-fast-relaxed-math" if env.fast_math else ""
+
+        try:
+            self._module = cl.Program(env.context, src).build(options=options)
+        except:
+            for i, l in enumerate(src.split('\n')):
+                print i, ":", l
+            raise
+
+    def __getattr__(self, name):
+        return OclKernel(self._env, getattr(self._module, name))
+
+
+class OclKernel:
+
+    def __init__(self, env, kernel):
+        self._env = env
+        self._kernel = kernel
+
+    def __call__(self, *args, **kwds):
+        # Python 2.* cannot handle explicit keywords after variable-length positional arguments
+        block = kwds.pop('block', (1, 1, 1))
+        grid = kwds.pop('grid', (1, 1))
+        shared = kwds.pop('shared', 0)
+        assert len(kwds) == 0, "Unknown keyword arguments: " + str(kwds.keys())
+
+        global_size = (block[0] * grid[0], block[1] * grid[1], block[2])
+
+        # Unlike PyCuda, PyOpenCL does not allow passing array objects as is
+        args = [x.data if isinstance(x, clarray.Array) else x for x in args]
+
+        self._kernel(self._env.queue, global_size, block, *args)
