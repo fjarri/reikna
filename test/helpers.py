240612ee2825fab8a755b2b2c82c84cee168f85e
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-04-30 00:30:52 +1000

    test/helpers: added support for complex test arrays

--- test/helpers.py
+++ test/helpers.py
@@ -1,7 +1,14 @@
 import numpy
+from tigger.cluda import dtypes
 
 def getTestArray(shape, dtype):
-    return numpy.random.normal(size=shape).astype(dtype)
+    dtype = numpy.dtype(dtype)
+    get_arr = lambda: numpy.random.normal(size=shape).astype(dtype)
+
+    if dtypes.is_complex(dtype):
+        return get_arr() + 1j * get_arr()
+    else:
+        return get_arr()
 
 def diff(m, m_ref):
     return numpy.linalg.norm(m - m_ref) / numpy.linalg.norm(m_ref)
