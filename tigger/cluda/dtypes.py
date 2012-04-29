c47331c7ce3822481acc23850d5386f7a9b13a26
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-04-30 00:30:34 +1000

    tigger/cluda: renamed helpers to dtypes

--- tigger/cluda/dtypes.py
+++ tigger/cluda/dtypes.py
@@ -1,17 +1,22 @@
 import numpy
 
 def is_complex(dtype):
-    return dtype.name in ['complex64', 'complex128']
+    return dtype.kind == 'c'
 
 def is_double(dtype):
     return dtype.name in ['float64', 'complex128']
 
 def ctype(dtype):
     return dict(
-        uint8='unsigned char',
         float32='float',
         float64='double',
+        complex64='float2',
+        complex128='double2'
     )[dtype.name]
+
+def complex_for_dtype(dtype):
+    x = cast(dtype)(1)
+    return (x + 1j * x).dtype
 
 def complex_ctr(dtype):
     return 'COMPLEX_CTR(' + ctype(dtype) + ')'
