cb5721f2dd227d6f0cc3e217b938d083866794a0
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-07-18 16:49:01 +1000

    test/test_cluda: added compilation test

--- test/test_cluda.py
+++ test/test_cluda.py
@@ -49,3 +49,28 @@ def test_connect_to_context_and_stream(cluda_api):
 	ctx2.release()
 
 	ctx.release()
+
+def test_compilation(ctx):
+
+	N = 256
+	coeff = 2
+
+	module = ctx.compile(
+	"""
+	KERNEL void multiply_them(GLOBAL_MEM float *dest, GLOBAL_MEM float *a, GLOBAL_MEM float *b)
+	{
+	  const int i = LID_0;
+	  dest[i] = ${func.mul(numpy.float32, numpy.float32)}(a[i], b[i]) * (float)${coeff};
+	}
+	""", coeff=coeff)
+
+	multiply_them = module.multiply_them
+
+	a = numpy.random.randn(N).astype(numpy.float32)
+	b = numpy.random.randn(N).astype(numpy.float32)
+	a_dev = ctx.to_device(a)
+	b_dev = ctx.to_device(b)
+	dest_dev = ctx.empty_like(a_dev)
+
+	multiply_them(dest_dev, a_dev, b_dev, block=(N,1,1), grid=(1,1))
+	assert diff(ctx.from_device(dest_dev), a * b * coeff) < SINGLE_EPS
