c3c3fd8466ad933dc2d390aa9c8ea525b93e23b5
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-07-31 11:00:04 +1000

    Added elementwise computation class and a simple test for it

---
+++ test/test_elementwise.py
@@ -0,0 +1,34 @@
+import numpy
+import pytest
+
+from helpers import *
+
+from tigger.elementwise import Elementwise
+import tigger.cluda.dtypes as dtypes
+
+def test_errors(ctx):
+
+    # variant 1: pass kernel & argument list in the constructor
+    elw = Elementwise(ctx,
+        """
+        ${ctype.input} a1 = ${load.input}(idx);
+        ${ctype.input} a2 = ${load.input}(idx + size);
+        ${store.output}(idx, a1 + a2 + ${param.param});
+        """,
+        (['output'], ['input'], ['param']))
+
+    N = 1000
+    a = get_test_array(N * 2, numpy.float32)
+    a_dev = ctx.to_device(a)
+    b_dev = ctx.allocate(N, numpy.float32)
+    param = 1
+
+    elw.prepare(
+        output_dtype=numpy.float32, input_dtype=numpy.float32,
+        param_dtype=numpy.float32, size=N)
+    elw(b_dev, a_dev, param)
+    assert diff_is_negligible(ctx.from_device(b_dev), a[:N] + a[N:] + param)
+
+    elw.prepare_for(b_dev, a_dev, numpy.float32(param))
+    elw(b_dev, a_dev, param)
+    assert diff_is_negligible(ctx.from_device(b_dev), a[:N] + a[N:] + param)
---
+++ tigger/elementwise.py
@@ -0,0 +1,62 @@
+import numpy
+from tigger.core import *
+
+TEMPLATE = template_for(__file__)
+
+
+class Elementwise(Computation):
+
+    def __init__(self, ctx, code, signature, **kwds):
+        self._code = code
+        self._base_stores, self._base_loads, self._base_params = signature
+        Computation.__init__(self, ctx, **kwds)
+
+    def _get_default_basis(self):
+        res = {(name + '_dtype'):numpy.float32 for name in
+            self._base_stores + self._base_loads + self._base_params}
+        res['size'] = 1
+        return res
+
+    def _construct_basis(self, *args):
+
+        bs = dict(size=args[0].size)
+
+        names = self._base_stores + self._base_loads + self._base_params
+        for arg, name in zip(args, names):
+            bs[name + '_dtype'] = arg.dtype
+
+        return bs
+
+    def _get_base_signature(self):
+        bs = self._basis
+        stores = [(name, ArrayValue(None, bs[name + '_dtype'])) for name in self._base_stores]
+        stores[0][1].shape = (bs.size,)
+        loads = [(name, ArrayValue(None, bs[name + '_dtype'])) for name in self._base_loads]
+        params = [(name, ScalarValue(None, bs[name + '_dtype'])) for name in self._base_params]
+
+        return stores, loads, params
+
+    def _construct_kernels(self):
+        bs = self._basis
+
+        template = template_from("""
+        KERNEL void elementwise(${signature})
+        {
+            int idx = ID_FLAT;
+            int size = ${size};
+            if (idx < ${size})
+            {
+        """ +
+        self._code +
+        """
+            }
+        }
+        """)
+
+        src = self._render(template, size=bs.size)
+
+        names = self._base_stores + self._base_loads + self._base_params
+        return [KernelCall(
+            'elementwise', names, src,
+            global_size=bs.size
+        )]
