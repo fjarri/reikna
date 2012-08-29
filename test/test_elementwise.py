f2950b5c4837e1f16a4947f9f9ce7932e990d410
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-08-29 23:32:42 +1000

    test/test_elementwise: added test for a kernel with some prelude

--- test/test_elementwise.py
+++ test/test_elementwise.py
@@ -33,18 +33,24 @@ def test_errors(ctx):
     elw(b_dev, a_dev, param)
     assert diff_is_negligible(ctx.from_device(b_dev), a[:N] + a[N:] + param)
 
-'''
-def atest_nontrivial_code(ctx):
 
-    elw = Elementwise(ctx)
+def test_nontrivial_code(ctx):
 
-    code = """
+    argnames = (('output',), ('input',), ('param',))
+    elw = Elementwise(ctx).set_argnames(*argnames)
+
+    code = dict(
+        kernel="""
         ${input.ctype} a1 = ${input.load}(idx);
-        ${input.ctype} a2 = ${input.load}(idx + size);
-        ${output.store}(idx, a1 + a2 + ${param});
-        """
-        ,
-        (['output'], ['input'], ['param']))
+        ${input.ctype} a2 = ${input.load}(idx + ${size});
+        ${output.store}(idx, a1 + test(a2, ${param}));
+        """,
+        functions="""
+        WITHIN_KERNEL ${output.ctype} test(${input.ctype} val, ${param.ctype} param)
+        {
+            return val + param;
+        }
+        """)
 
     N = 1000
     a = get_test_array(N * 2, numpy.float32)
@@ -52,7 +58,6 @@ def atest_nontrivial_code(ctx):
     b_dev = ctx.allocate(N, numpy.float32)
     param = 1
 
-    elw.prepare_for(b_dev, a_dev, numpy.float32(param))
+    elw.prepare_for(b_dev, a_dev, numpy.float32(param), code=code)
     elw(b_dev, a_dev, param)
     assert diff_is_negligible(ctx.from_device(b_dev), a[:N] + a[N:] + param)
-'''
