91e6cbf469d94669621584a961d4b24ac4c56afc
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-09-06 11:42:22 +1000

    cluda/dtypes: attempting to fix rare error caused by different float multiplication on GPU and CPU

--- tigger/cluda/dtypes.py
+++ tigger/cluda/dtypes.py
@@ -117,7 +117,7 @@ def c_constant(val, dtype=None):
     elif is_integer(dtype):
         return str(val) + ("L" if dtype.itemsize > 4 else "")
     else:
-        return str(val) + ("f" if dtype.itemsize <= 4 else "")
+        return str(float(val)) + ("f" if dtype.itemsize <= 4 else "")
 
 def _register_dtype(dtype, ctype):
     dtype = normalize_type(dtype)
