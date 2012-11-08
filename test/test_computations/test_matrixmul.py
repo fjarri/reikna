7834236407b12100ade8f9c68eb785dd833c5401
Bogdan Opanchuk <bogdan@opanchuk.net>
2012-11-08 13:19:14 +1100

    test_matrixmul: added some non-performance tests

--- test/test_computations/test_matrixmul.py
+++ test/test_computations/test_matrixmul.py
@@ -12,12 +12,6 @@ from tigger.helpers import product
 
 def pytest_generate_tests(metafunc):
 
-    perf_log_shapes = [
-        (4,), (10,), (13,), # 1D
-        (4, 4), (7, 7), (10, 10), # 2D
-        (4, 4, 4), (5, 5, 7), (7, 7, 7)] # 3D
-    perf_mem_limit = 4 * 2**20
-
     if 'perf_bwo' in metafunc.funcargnames:
         bwos = [8, 16, 32]
         ids=["8x8", "16x16", "32x32"]
@@ -29,7 +23,7 @@ def pytest_generate_tests(metafunc):
         for s in [(64, 64), (256, 256), (1024, 1024)]:
             shapes.append((s, s))
 
-        for s in [(1024, 16, 16), (256, 32, 32)]:
+        for s in [(1024, 16, 16), (256, 32, 32), (512, 25, 25)]:
             shapes.append((s, s[1:]))
             shapes.append((s, s))
 
@@ -38,6 +32,26 @@ def pytest_generate_tests(metafunc):
             ids.append(str(s[0]) + 'x' + str(s[1]))
 
         metafunc.parametrize('perf_shapes', shapes, ids=ids)
+
+    if 'shapes' in metafunc.funcargnames:
+
+        shapes = [
+            ((15, 17), (17, 3)),
+            ((122, 5), (5, 1000)),
+            ((45, 99), (30, 99, 40)),
+            ((12, 56, 78), (12, 78, 44))
+        ]
+
+        ids = [str(s1) + 'x' + str(s2) for s1, s2 in shapes]
+
+        metafunc.parametrize('shapes', shapes, ids=ids)
+
+    if 'arg_dtypes' in metafunc.funcargnames:
+
+        arg_dtypes = [(False, False), (False, True), (True, False), (True, True)]
+        mark = lambda x: 'c' if x else 'r'
+        ids = [mark(t1) + mark(t2) for t1, t2 in arg_dtypes]
+        metafunc.parametrize('arg_dtypes', arg_dtypes, ids=ids)
 
 
 def ref_dot(a, b):
@@ -65,30 +79,28 @@ def ref_dot(a, b):
     return out
 
 
-@pytest.mark.parametrize("complex1", [False, True], ids=['real', 'complex'])
-@pytest.mark.parametrize("complex2", [False, True], ids=['real', 'complex'])
-def test_errors(ctx_and_double, complex1, complex2):
+def test_errors(ctx_and_double, shapes, arg_dtypes):
 
     ctx, double = ctx_and_double
-
-    s1 = (100, 200)
-    s2 = (200, 100)
+    s1, s2 = shapes
+    c1, c2 = arg_dtypes
 
     dtype = numpy.float64 if double else numpy.float32
-    dtype1 = dtypes.complex_for(dtype) if complex1 else dtype
-    dtype2 = dtypes.complex_for(dtype) if complex2 else dtype
-    res_dtype = numpy.result_type(dtype1, dtype2)
+    dtype1 = dtypes.complex_for(dtype) if c1 else dtype
+    dtype2 = dtypes.complex_for(dtype) if c2 else dtype
 
     a = get_test_array(s1, dtype1)
     b = get_test_array(s2, dtype2)
+    res_ref = ref_dot(a, b)
 
     a_dev = ctx.to_device(a)
     b_dev = ctx.to_device(b)
-    res_dev = ctx.allocate((s1[0], s2[1]), dtype=res_dtype)
+    res_dev = ctx.empty_like(res_ref)
+
     dot = MatrixMul(ctx).prepare_for(res_dev, a_dev, b_dev)
     dot(res_dev, a_dev, b_dev)
 
-    assert diff_is_negligible(ctx.from_device(res_dev), numpy.dot(a, b))
+    assert diff_is_negligible(ctx.from_device(res_dev), res_ref)
 
 
 def check_performance(ctx_and_double, shape1, shape2, bwo):
