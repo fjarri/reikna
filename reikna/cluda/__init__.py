46098b5a39da4666dc6b730c3207b16fabfcb20b
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-04-20 23:28:01 +1000

    cluda: documented OutOfResourcesError exception

--- reikna/cluda/__init__.py
+++ reikna/cluda/__init__.py
@@ -3,4 +3,8 @@ from reikna.cluda.kernel import Module
 
 
 class OutOfResourcesError(Exception):
+    """
+    Thrown by :py:meth:`~reikna.cluda.api.Context.compile_static`
+    if the provided ``local_size`` is too big, or one cannot be found.
+    """
     pass
