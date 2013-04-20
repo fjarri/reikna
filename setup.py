b2febac0df229f6de19a147a0c55d11ac3beeca8
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-04-20 17:11:17 +1000

    setup: next version is 0.2.3

--- setup.py
+++ setup.py
@@ -89,8 +89,8 @@ if not release:
     version = full_version
     '''
 
-    VERSION = (0, 2, 2)
-    RELEASED = True
+    VERSION = (0, 2, 3)
+    RELEASED = False
 
     version_str = '%d.%d.%d' % VERSION
     full_version_str = version_str
