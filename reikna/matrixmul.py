239fb59087bc1aac790cb9cc18ccdefc747dd80a
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-07-25 15:45:01 +1000

    matrixmul: pylint pass

--- reikna/matrixmul.py
+++ reikna/matrixmul.py
@@ -1,12 +1,10 @@
-import numpy
-
-from reikna.helpers import *
-from reikna.core import *
+import reikna.helpers as helpers
+from reikna.core import Computation, Parameter, Annotation, Type
 import reikna.cluda.dtypes as dtypes
 from reikna.cluda import OutOfResourcesError
 from reikna.cluda import functions
 
-TEMPLATE = template_for(__file__)
+TEMPLATE = helpers.template_for(__file__)
 
 
 class MatrixMul(Computation):
@@ -28,7 +26,7 @@ class MatrixMul(Computation):
         if out_arr is None:
             out_dtype = dtypes.result_type(a_arr.dtype, b_arr.dtype)
 
-            a_batch = product(a_arr.shape[:-2])
+            a_batch = helpers.product(a_arr.shape[:-2])
             batch_len = max(len(a_arr.shape[:-2]), len(b_arr.shape[:-2]))
             batch_shape = b_arr.shape[:-2] if a_batch == 1 else a_arr.shape[:-2]
             batch_shape = (1,) * (batch_len - len(batch_shape)) + batch_shape
@@ -51,13 +49,13 @@ class MatrixMul(Computation):
             block_widths = [bwo]
         else:
             nbanks = device_params.local_mem_banks
-            block_widths = [2 ** n for n in range(log2(nbanks), -1, -1)]
+            block_widths = [2 ** n for n in range(helpers.log2(nbanks), -1, -1)]
 
         a_height = a.shape[-2]
         a_width = a.shape[-1]
         b_width = b.shape[-1]
-        a_batch = product(a.shape[:-2])
-        b_batch = product(b.shape[:-2])
+        a_batch = helpers.product(a.shape[:-2])
+        b_batch = helpers.product(b.shape[:-2])
         batch = max(a_batch, b_batch)
 
         for block_width in block_widths:
@@ -67,9 +65,9 @@ class MatrixMul(Computation):
             if block_width ** 2 > device_params.max_work_group_size:
                 continue
 
-            num_steps = min_blocks(a_width, block_width)
-            blocks_per_matrix = min_blocks(a_height, block_width)
-            grid_width = min_blocks(b_width, block_width)
+            num_steps = helpers.min_blocks(a_width, block_width)
+            blocks_per_matrix = helpers.min_blocks(a_height, block_width)
+            grid_width = helpers.min_blocks(b_width, block_width)
 
             render_kwds = dict(
                 batched_a=(a_batch != 1),
