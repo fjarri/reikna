52d74fb80630c5f38890ef2c2fa215fff5183be2
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-07-25 16:11:33 +1000

    reduce: pylint pass

--- reikna/reduce.py
+++ reikna/reduce.py
@@ -1,12 +1,10 @@
-import numpy
-
 from reikna.cluda import Snippet
-from reikna.helpers import *
+import reikna.helpers as helpers
 from reikna.cluda import dtypes
-from reikna.core import *
+from reikna.core import Computation, Parameter, Annotation, Type
 from reikna.transpose import Transpose
 
-TEMPLATE = template_for(__file__)
+TEMPLATE = helpers.template_for(__file__)
 
 
 class Predicate:
@@ -45,11 +43,11 @@ class Reduce(Computation):
         self._axis_end = axes[-1]
         self._predicate = predicate
 
-        Computation.__init__(self,[
+        Computation.__init__(self, [
             Parameter('output', Annotation(Type(arr_t.dtype, shape=output_shape), 'o')),
             Parameter('input', Annotation(arr_t, 'i'))])
 
-    def _build_plan(self, plan_factory, device_params, output, input):
+    def _build_plan(self, plan_factory, device_params, output, input_):
 
         plan = plan_factory()
 
@@ -59,40 +57,40 @@ class Reduce(Computation):
         axis_start = self._axis_start
         axis_end = self._axis_end
 
-        if axis_end == len(input.shape) - 1:
+        if axis_end == len(input_.shape) - 1:
             # normal reduction
-            cur_input = input
+            cur_input = input_
         else:
-            initial_axes = tuple(range(len(input.shape)))
+            initial_axes = tuple(range(len(input_.shape)))
             tr_axes = (
                 initial_axes[:axis_start] +
                 initial_axes[axis_end+1:] +
                 initial_axes[axis_start:axis_end+1])
 
-            transpose = Transpose(input, axes=tr_axes)
+            transpose = Transpose(input_, axes=tr_axes)
             tr_output = plan.temp_array_like(transpose.parameter.output)
-            plan.computation_call(transpose, tr_output, input)
+            plan.computation_call(transpose, tr_output, input_)
 
             cur_input = tr_output
-            axis_start = len(input.shape) - 1 - (axis_end - axis_start)
-            axis_end = len(input.shape) - 1
+            axis_start = len(input_.shape) - 1 - (axis_end - axis_start)
+            axis_end = len(input_.shape) - 1
 
         input_slices = (axis_start, axis_end - axis_start + 1)
 
         external_shape = cur_input.shape[:axis_start]
-        part_size = product(cur_input.shape[axis_start:])
-        final_size = product(external_shape)
+        part_size = helpers.product(cur_input.shape[axis_start:])
+        final_size = helpers.product(external_shape)
 
         while part_size > 1:
 
             if part_size >= max_reduce_power:
                 block_size = max_reduce_power
-                blocks_per_part = min_blocks(part_size, block_size)
+                blocks_per_part = helpers.min_blocks(part_size, block_size)
                 cur_output = plan.temp_array(
-                    (final_size, blocks_per_part), input.dtype)
+                    (final_size, blocks_per_part), input_.dtype)
                 output_slices = (1, 1)
             else:
-                block_size = bounding_power_of_2(part_size)
+                block_size = helpers.bounding_power_of_2(part_size)
                 blocks_per_part = 1
                 cur_output = output
                 output_slices = (len(cur_output.shape), 0)
@@ -105,7 +103,7 @@ class Reduce(Computation):
             render_kwds = dict(
                 blocks_per_part=blocks_per_part,
                 last_block_size=last_block_size,
-                log2=log2, block_size=block_size,
+                log2=helpers.log2, block_size=block_size,
                 warp_size=device_params.warp_size,
                 predicate=self._predicate,
                 input_slices=input_slices,
