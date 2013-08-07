8ef033b60c163060f6748521db4a74c25d30cd5b
Bogdan Opanchuk <bogdan@opanchuk.net>
2013-08-07 10:40:03 +1000

    setup: added more specific Python version classifiers

--- setup.py
+++ setup.py
@@ -157,6 +157,12 @@ if __name__ == '__main__':
             'Intended Audience :: Developers',
             'License :: OSI Approved :: MIT License',
             'Programming Language :: Python',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.6',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: Python :: 3.2',
+            'Programming Language :: Python :: 3.3',
             'Topic :: Software Development',
             'Topic :: Scientific/Engineering',
             'Operating System :: OS Independent'
