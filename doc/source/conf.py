d393e0f85d69664d7eeb1f09aad9e8d6ed54ca83
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-02-20 12:44:02 +1100

    doc/source/conf: updated the copyright message

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'reikna'
-copyright = '2012-2013, Bogdan Opanchuk'
+copyright = '2012-2014, Bogdan Opanchuk'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
