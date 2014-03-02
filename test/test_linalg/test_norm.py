71a5c387deb4ca29834fb586b90d37bbb6ca579a
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-03-02 17:35:43 +1100

    test/linalg/norm: added a reference function supporting custom axes and generalised the test

--- test/test_linalg/test_norm.py
+++ test/test_linalg/test_norm.py
@@ -5,19 +5,38 @@ from helpers import *
 from reikna.linalg import EntrywiseNorm
 
 
-@pytest.mark.parametrize('dtype', [numpy.float32, numpy.complex64], ids=['float32', 'complex64'])
-@pytest.mark.parametrize('order', [0.5, 1, 2])
-def test_entrywise_norm(thr, dtype, order):
+def reference_norm(arr, order=1, axes=None):
+    if axes is None:
+        axes = list(range(len(arr.shape)))
 
-    a = get_test_array(1000, dtype)
+    arr = numpy.abs(arr)
+    out_dtype = arr.dtype
+
+    arr = arr ** order
+    for axis in reversed(sorted(axes)):
+        arr = arr.sum(axis)
+
+    # explicit cast to preven numpy promoting arr to float64 from float32 if it is 0-dimensional
+    res = arr ** numpy.cast[out_dtype](1. / order)
+    return res
+
+
+def check_norm(thr, shape, dtype, order, axes):
+    a = get_test_array(shape, dtype)
     a_dev = thr.to_device(a)
 
-    norm = EntrywiseNorm(a_dev, order=order)
+    norm = EntrywiseNorm(a_dev, order=order, axes=axes)
 
     b_dev = thr.empty_like(norm.parameter.output)
-    b_ref = numpy.linalg.norm(a, ord=order).astype(norm.parameter.output.dtype)
+    b_ref = reference_norm(a, order=order, axes=axes)
 
     normc = norm.compile(thr)
     normc(b_dev, a_dev)
 
     assert diff_is_negligible(b_dev.get(), b_ref)
+
+
+@pytest.mark.parametrize('dtype', [numpy.float32, numpy.complex64], ids=['float32', 'complex64'])
+@pytest.mark.parametrize('order', [0.5, 1, 2])
+def test_all_axes(thr, dtype, order):
+    check_norm(thr, 100, dtype, order, None)
