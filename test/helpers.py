fbe33e7203cc42d3133d03c43f0db2bbc36a2816
Bogdan Opanchuk <bogdan@opanchuk.net>
2014-09-25 11:29:17 +1000

    test/helpers: using numpy.allclose() to check the equality of arrays in tests

--- test/helpers.py
+++ test/helpers.py
@@ -4,8 +4,15 @@ from numpy.lib.stride_tricks import as_strided
 from reikna.helpers import wrap_in_tuple
 from reikna.cluda import dtypes
 
-SINGLE_EPS = 1e-6
-DOUBLE_EPS = 1e-11
+
+# Default tolerances for numpy.allclose().
+# Should be enough to detect a error, but not enough to trigger a fail
+# in case of a slightly imprecise implementation of some operation on a GPU.
+SINGLE_RTOL = 1e-5
+SINGLE_ATOL = 1e-8
+
+DOUBLE_RTOL = 1e-11
+DOUBLE_ATOL = 1e-11
 
 
 def get_test_array_like(arr, **kwds):
@@ -42,10 +49,8 @@ def get_test_array(shape, dtype, strides=None, no_zeros=False, high=None):
 
     return result
 
-def float_diff(m, m_ref):
-    return numpy.linalg.norm(m - m_ref) / numpy.linalg.norm(m_ref)
 
-def diff_is_negligible(m, m_ref):
+def diff_is_negligible(m, m_ref, atol=None, rtol=None):
 
     if m.dtype.names is not None:
         return all(diff_is_negligible(m[name], m_ref[name]) for name in m.dtype.names)
@@ -53,10 +58,22 @@ def diff_is_negligible(m, m_ref):
     assert m.dtype == m_ref.dtype
 
     if dtypes.is_integer(m.dtype):
-        return ((m - m_ref) == 0).all()
-
-    diff = float_diff(m, m_ref)
-    if dtypes.is_double(m.dtype):
-        return diff < DOUBLE_EPS
+        close = (m == m_ref)
     else:
-        return diff < SINGLE_EPS
+        if atol is None:
+            atol = DOUBLE_ATOL if dtypes.is_double(m.dtype) else SINGLE_ATOL
+        if rtol is None:
+            rtol = DOUBLE_RTOL if dtypes.is_double(m.dtype) else SINGLE_RTOL
+
+        close = numpy.isclose(m, m_ref, atol=atol, rtol=rtol)
+
+    if close.all():
+        return True
+
+    far_idxs = numpy.vstack(numpy.where(close == False)).T
+    print("diff_is_negligible() found " + str(far_idxs.shape[0]) + " differences, first ones are:")
+    for idx, _ in zip(far_idxs, range(10)):
+        print("idx: {idx}, test: {test}, ref: {ref}".format(
+            idx=idx, test=m[tuple(idx)], ref=m_ref[(tuple(idx))]))
+
+    return False
